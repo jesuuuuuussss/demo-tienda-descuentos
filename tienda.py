53ffa35d1eed9a6e77c12240f06f7c5d22e52e9e
jesuuuuuussss <cuquin776@gmail.com>
2026-09-21 18:09:35 -0600

    Update tienda.py
    Aplicar descuento del 10 por ciento en compras desde 300

--- tienda.py
+++ tienda.py
@@ -3,5 +3,13 @@ cantidad = 2
 
 subtotal = precio * cantidad
 
+if subtotal >= 300:
+    descuento = subtotal * 0.10
+else:
+    descuento = 0
+
+total = subtotal - descuento
+
 print(f"Subtotal: ${subtotal:.2f}")
-print(f"Total a pagar: ${subtotal:.2f}")
+print(f"Descuento: ${descuento:.2f}")
+print(f"Total a pagar: ${total:.2f}")
